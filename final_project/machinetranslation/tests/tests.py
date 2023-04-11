650bb1601cd7fcccf5f3a74cfdd2a3629b66c68d
tsapekpa <tsapekpa@gmail.com>
2023-04-11 02:11:17 -0400

    Final changes

--- final_project/machinetranslation/tests/tests.py
+++ final_project/machinetranslation/tests/tests.py
@@ -4,12 +4,12 @@ from translator import french_to_english, english_to_french
 
 class frenchToEnglish(unittest.TestCase):
     def test1(self):
-        self.assertEqual(french_to_english(), 'Vous devez fournir du texte') # test when null is given. 
+        self.assertNotEqual(french_to_english(), '') # test when null is given. 
         self.assertEqual(french_to_english('Bonjour'), 'Hello')  # test when 'Bonjour' is given as input the output is 'Hello'
 
 class englishToFrench(unittest.TestCase):
     def test1(self):
-        self.assertEqual(english_to_french(), 'You need to provide text') # test when null is given.
+        self.assertNotEqual(english_to_french(), '') # test when null is given.
         self.assertEqual(english_to_french('Hello'), 'Bonjour' ) # test when 'Hello' is given as input the output is 'Bonjour'
 
 unittest.main()
